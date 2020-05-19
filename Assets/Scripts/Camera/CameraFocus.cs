02368bc8594877140a92474e83d33626c07b2d67
steve628 <s0558040@htw-berlin.de>
2020-05-19 20:18:32 +0200

    Set camera mode to continuous auto and add tourch function

---
+++ Assets/Scripts/Camera/CameraFocus.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+/**
+ * Foreign Code
+ * Source: https://library.vuforia.com/content/vuforia-library/en/articles/Solution/Working-with-the-Camera.html
+ * Script to set camera focus mode to continuous auto
+ */
+public class CameraFocus : MonoBehaviour
+{
+    void Start()
+    {
+        VuforiaARController vuforia = VuforiaARController.Instance;
+        vuforia.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+        vuforia.RegisterOnPauseCallback(OnPaused);
+    }
+
+    private void OnVuforiaStarted()
+    {
+        CameraDevice.Instance.SetFocusMode(
+            CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    }
+
+    private void OnPaused(bool paused)
+    {
+        if (!paused) // resumed
+        {
+            // Set again autofocus mode when app is resumed
+            CameraDevice.Instance.SetFocusMode(
+                CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        }
+    }
+}
---
+++ Assets/Scripts/Camera/CameraTourch.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+
+/**
+ * @author Stefan Manthey
+ * Script to toogle the smartphone tourch on and off
+ */
+public class CameraTourch : MonoBehaviour
+{
+    public Text tourchText;
+    private bool _isActive;
+
+    private void Start()
+    {
+        tourchText.text = "Activate Tourch";
+        _isActive = false;
+    }
+
+    public void SwitchTourch()
+    {
+        if(_isActive)
+        {
+            DeactivateTourch();
+        } else
+        {
+            ActivateTourch();
+        }
+
+        _isActive = !_isActive;
+    }
+
+    private void ActivateTourch() {
+        CameraDevice.Instance.SetFlashTorchMode(true);
+        tourchText.text = "Deactivate Tourch";
+    }
+
+    private void DeactivateTourch() {
+        CameraDevice.Instance.SetFlashTorchMode(false);
+        tourchText.text = "Activate Tourch";
+    }
+}
